2b585a4f7b8782e035390578be87b2e1d712ed58
mayonaiys <49065703+mayonaiys@users.noreply.github.com>
2020-03-18 15:01:47 +0100

    Exercice2

---
+++ Fils.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Remi on 18/03/2020.
+//
+
+#include "Fils.h"
---
+++ Fils.h
@@ -0,0 +1,14 @@
+//
+// Created by Remi on 18/03/2020.
+//
+
+#ifndef TP3_FILS_H
+#define TP3_FILS_H
+
+
+class Fils {
+
+};
+
+
+#endif //TP3_FILS_H
